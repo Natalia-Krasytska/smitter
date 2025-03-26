e25f68c4d43e98359fa8fbd9e654d3635b6be9a0
natal <natalie77335@gmail.com>
2025-03-26 12:47:33 +0100

    First Commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className={`content`}>
+            Main Content here
+
+        </div>
+    );
+};
+
+export default Content;
