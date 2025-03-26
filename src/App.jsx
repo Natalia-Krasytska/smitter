4df4dae1c4c5f09452abe326a8031329e319553d
natal <natalie77335@gmail.com>
2025-03-26 19:41:30 +0100

    update Avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import Navigation from "./components/Navigation.jsx";
 import Body from "./components/Body.jsx";
-import './App.css'
-import {useState} from "react";
-import {TwitterContext} from "./utils/Context.js";
+import { useState } from "react";
+import { TwitterContext } from "./utils/Context.js";
+import Avatar from './components/Avatar';
 
 function App() {
     const [user, setUser] = useState({
         name: 'Monster',
         avatar: 'https://gravatar.com/avatar/000?d=monsterid'
-
     });
     const [stats, setStats] = useState({
         followers: 10,
@@ -17,16 +16,28 @@ function App() {
     });
 
 
+    const updateAvatar = (newAvatarUrl) => {
+        if (newAvatarUrl.trim()) {
+            setUser((prevUser) => ({
+                ...prevUser,
+                avatar: newAvatarUrl
+            }));
+        }
+    };
+
     return (
         <div className={`app`}>
-            <TwitterContext.Provider value={{
-                user, stats
-            }}>
+            <TwitterContext.Provider value={{ user, stats }}>
                 <Navigation />
                 <Body />
+                {}
+                <Avatar
+                    avatar={user.avatar}
+                    updateAvatar={updateAvatar}
+                />
             </TwitterContext.Provider>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,69 @@
-import React, {useContext} from 'react';
-import {TwitterContext} from "../utils/Context.js";
+import React, { useState } from 'react';
+
+const Avatar = ({ avatar, updateAvatar }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newAvatarUrl, setNewAvatarUrl] = useState(avatar);
 
 
-const Avatar = ({size}) => {
-    const {user} = useContext(TwitterContext);
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+
+    const handleInputChange = (e) => {
+        setNewAvatarUrl(e.target.value);
+    };
+
+
+    const handleSubmit = () => {
+        updateAvatar(newAvatarUrl);
+        closeModal();
+    };
+
+
+    const handleCancel = () => {
+        closeModal();
+    };
+
     return (
-       <img className={`user-avatar ${size || ''}`} src={user.avatar} alt={user.name}/>
+        <div>
+            <img
+                className="user-avatar"
+                src={avatar}
+                alt="User Avatar"
+                style={{ width: '100px', height: '100px', borderRadius: '50%' }}
+                onClick={openModal}
+            />
+
+
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h3>Enter the new Avatar URL</h3>
+                        <input
+                            type="text"
+                            value={newAvatarUrl}
+                            onChange={handleInputChange}
+                            placeholder="Enter new Avatar URL"
+                        />
+                        <div>
+                            <button onClick={handleSubmit}>OK</button>
+                            <button onClick={handleCancel}>Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
 export default Avatar;
+
+
+
+
