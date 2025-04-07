f8a48d18a8ea6b53c6a1c8b5fe740092371f2bc6
natal <natalie77335@gmail.com>
2025-04-07 18:57:57 +0200

    reducerSplitting

---
+++ src/Reducer/statsReduser.js
@@ -0,0 +1,17 @@
+import { CHANGE_STATS } from "../action/statsAction.js";
+
+const initialStatsState = {
+    followers: 0,
+    following: 0
+};
+
+export const statsReducer = (state = initialStatsState, action) => {
+    switch (action.type) {
+        case CHANGE_STATS: {
+            const res = state[action.payload.statsType] + action.payload.sum;
+            return { ...state, [action.payload.statsType]: res >= 0 ? res : 0 };
+        }
+        default:
+            return state;
+    }
+};
