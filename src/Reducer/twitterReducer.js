beca7bddd2267385dedd42193b2cd4f8045a58b4
natal <natalie77335@gmail.com>
2025-04-07 18:57:22 +0200

    reducerSplitting

--- src/Reducer/twitterReducer.js
+++ src/Reducer/twitterReducer.js
@@ -1,18 +1,17 @@
-import {CHANGE_AVATAR, CHANGE_NAME} from "../action/userAction.js";
-import {CHANGE_STATS} from "../action/statsAction.js";
+import { CHANGE_AVATAR, CHANGE_NAME } from "../action/userAction.js";
 
-export const twitterReducer = (state, action) => {
+const initialUserState = {
+    name: 'Monster',
+    avatar: 'https://gravatar.com/avatar/000?d=monsterid'
+};
+
+export const userReducer = (state = initialUserState, action) => {
     switch (action.type) {
         case CHANGE_AVATAR:
-            return {...state, user: {...state.user, avatar: action.payload || state.user.avatar}};
-         case CHANGE_NAME:
-            return {...state, user: {...state.user, name: action.payload || state.user.name}};
-        case CHANGE_STATS: {
-            const res = state.stats[action.payload.statsType] + action.payload.sum;
-            const stats = {...state.stats, [action.payload.statsType]: res >= 0 ? res : 0};
-            return {...state, stats};
-        }
+            return { ...state, avatar: action.payload || state.avatar };
+        case CHANGE_NAME:
+            return { ...state, name: action.payload || state.name };
         default:
             return state;
     }
-}
+};
--- src/configureStore/store.js
+++ src/configureStore/store.js
@@ -1,17 +1,4 @@
-import {legacy_createStore as createStore} from "redux";
-import {twitterReducer} from "../Reducer/twitterReducer.js";
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "../reducers/rootReducer";
 
-
-const initialState = {
-    user: {
-        name: 'Monster',
-        avatar: 'https://gravatar.com/avatar/000?d=monsterid'
-    },
-
-    stats: {
-        followers: 0,
-        following: 0
-    }
-}
-
-export const store = createStore(twitterReducer,initialState)
+export const store = createStore(rootReducer);
