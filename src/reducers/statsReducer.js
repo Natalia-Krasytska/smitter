3c1e65a2906bbd4ac55a93f6ccab636b614d97e6
natal <natalie77335@gmail.com>
2025-04-08 10:07:07 +0200

    splittingReducer

---
+++ src/reducers/statsReducer.js
@@ -0,0 +1,17 @@
+import {CHANGE_STATS} from "../actions/statsAction.js";
+
+/* type Stats = {
+    followers: number,
+    following: number,
+} */
+
+export const statsReducer = (stats, action) => {
+    switch (action.type) {
+        case CHANGE_STATS:{
+            const res = stats[action.payload.statsType] + action.payload.sum;
+            return {...stats, [action.payload.statsType]: res >= 0 ? res : 0};
+        }
+        default:
+            return stats;
+    }
+}
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,17 @@
+import {CHANGE_AVATAR, CHANGE_NAME} from "../actions/userAction.js";
+
+/* type User = {
+    avatar: string,
+    name: string
+} */
+
+export const userReducer = (state, action) => {
+    switch (action.type) {
+        case CHANGE_AVATAR:
+            return {...state, avatar: action.payload || state.avatar}
+        case CHANGE_NAME:
+            return {...state, name: action.payload || state.name}
+        default:
+            return state;
+    }
+}
